a0f6fb9c98a652de530d3f4b635b73824af04619
1131446340aa <1131446340@qq.com>
2021-01-25 02:50:01 +0800

    feat:all

--- sailing/src/common/getPos.tsx
+++
@@ -1,6 +0,0 @@
-function getPos(currentEle: HTMLElement, e: MouseEvent) {
-  currentEle.style.top = e.clientY + 'px'
-  currentEle.style.left = e.clientX + 'px'
-}
-
-export default getPos
--- sailing/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App'
-import router from './router'
-import store from './store'
-import './main.less'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-createApp(App).use(store).use(router).use(Antd).mount('#app')
